206d56026e4723ac23ccb3ae068f0c0d46a836bd
Gaurab Subedi <gaurabsubedi@Gaurabs-MacBook-Air.local>
2025-05-02 13:27:54 +0545

    model as done

---
+++ src/model/index.js
@@ -0,0 +1,17 @@
+import { Customer, DeliveryPartner, Admin } from "./user";
+import Branch from "./branch";
+import Product from "./products";
+import Category from "./category";
+import Order from "./order";
+import Counter from "./counter";
+
+export{
+    Order,
+    Category,
+    Counter,
+    Branch,
+    Product, 
+    DeliveryPartner,
+    Admin,
+    Customer
+}
