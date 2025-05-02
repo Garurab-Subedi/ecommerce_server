bf903cae04c380e56c8ced3ebc0bce0efb2fc95c
Gaurab Subedi <gaurabsubedi@Gaurabs-MacBook-Air.local>
2025-05-02 16:58:02 +0545

    some controller has added

---
+++ src/controllers/product/category.js
@@ -0,0 +1,10 @@
+import Category  from "../../model";
+
+export const getAllCategories =  async(req, reply) => {
+    try{
+        const categories = await Category.find();
+        return reply.send(categories);
+    }catch(error){
+        return reply.status(500).send({ message:"An error accurred", error})
+    }
+}
