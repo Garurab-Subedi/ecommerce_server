7390e638a6a689d90277f7be5b8a04df9160c21a
Gaurab Subedi <gaurabsubedi@Gaurabs-MacBook-Air.local>
2025-05-03 09:42:21 +0545

    admin has connected

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from './src/config/configure.js';
 import { connectDB } from './src/config/connect.js';
 import fastifySocketIO from 'fastify-socket.io';
 import { registerRoutes } from './src/routes/index.js';
+import { admin, buildAdminRouter } from './src/config/setup.js';
 
 const start = async () => {
    await connectDB(process.env.MONGO_URL);
@@ -20,12 +21,13 @@ const start = async () => {
 
    await registerRoutes(app);
 
+   await buildAdminRouter(app);
 
    app.listen({port:PORT,host:'0.0.0.0'},(err,addr)=>{
     if(err){
         console.log(err);
     }else{
-        console.log(`Ecommerce app running on http://localhost:${PORT}`)
+        console.log(`Ecommerce app running on http://localhost:${PORT}${admin.options.rootPath}`);
     }
    });
 
--- src/config/configure.js
+++ src/config/configure.js
@@ -1,2 +1,38 @@
+import fastifySession from "@fastify/session";
+import ConnectMongoDBSession from "connect-mongodb-session";
+import "dotenv/config";
+
 export const PORT = process.env.PORT || 3000;
 export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+
+const MongoDBStore = ConnectMongoDBSession(fastifySession);
+
+export const sessionStore = new MongoDBStore({
+    uri: process.env.MONGO_URL,
+    collection: "sessions",
+});
+
+sessionStore.on("error", (error) => {
+    console.log("Session store error:", error);
+});
+
+export const authenticate = async(email, password) => {
+    if(email && password){
+        if(email=="gaurab12@gmail.com" && password=="123456"){
+            return Promise.resolve({email: email, password: password});
+        }else{
+            return null;
+        }
+    }
+    // if(email && password){
+    //     const user = await Admin.findOne({ email });
+    //     if(!user){
+    //         return null;
+    //     }
+    //     if(user.password === password){
+    //         return Promise.resolve({email: email, password: password});
+    //     }else{
+    //     return null;
+    //     }
+    // }   
+}
